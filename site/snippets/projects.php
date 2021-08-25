2d14f3e463561788f2cdf8572e9ff04df197f6e3
Oskar <oskarradonkimball@gmail.com>
2021-08-25 13:58:09 -0700

    Update projects.php

--- site/snippets/projects.php
+++ site/snippets/projects.php
@@ -8,18 +8,20 @@ $projects = $data->children()->listed();
   <!-- <h1><?= $data->title() ?></h1> -->
 
     <?php foreach ($projects as $project): ?>
-      <!-- <a href="<?= $project->url() ?>"> -->
+      <a href="<?= $project->url() ?>">
         <figure <?php if( $project->category() == "Web application"): ?>class="app"<?php endif ?> >
           <div class="img-wrapper">
-            <img src="<?= $project->image()->resize(800)->url() ?>" class="<?= $project->image()->orientation()?>" />
+            <?php if ($cover = $project->cover()->toFile()): ?>
+              <img src="<?= $cover->resize(1000)->url() ?>" class="<?= $cover->orientation()?>" />
+            <?php endif ?>
           </div>
           <figcaption>
             <span class="title"><?= $project->title() ?></span>
             <span class="category"><?= $project->category() ?></span>
-            <span class="year"><?= $project->year() ?></span>
+            <span class="year"><?= $project->year()->toDate('m/Y') ?></span>
           </figcaption>
         </figure>
-      <!-- </a> -->
+      </a>
     <?php endforeach ?>
 
 </main>
