ad6a2db0a6c5c3190053a936c070fa013b4dfbcc
Oskar <oskarradonkimball@gmail.com>
2021-08-25 13:58:05 -0700

    Update project.php

--- site/templates/project.php
+++ site/templates/project.php
@@ -1,51 +1,66 @@
 <?php snippet('header'); ?>
 <main id="project">
- 
-  <h1><?= $page->title() ?></h1>
+
+  <header class="project-header">
+    <h1><?= $page->title() ?></h1>
+    <h4><?= $page->tagline() ?></h4>
+  </header>
+  
+  <?= $page->blocks()->toBlocks() ?>
+  <nav id="prev-next">
+    <?php if ($page->hasPrevListed()): ?>
+      <a id="prev" href="<?= $page->prevListed()->url() ?>">Previous project</a>
+    <?php endif ?>
+
+    <?php if ($page->hasNextListed()): ?>
+      <a id="next" href="<?= $page->nextListed()->url() ?>">Next project</a>
+    <?php endif ?>
+  </nav>
 
   <article class="info">
-    <div>
-      <h3>Year</h3>
-      <p><?= $page->year() ?></p>
-    </div>
-    <div>
-      <h3>Category</h3>
-      <?php foreach ($page->category()->split() as $category): ?>
-        <p><?= $category ?></p>
-      <?php endforeach ?>
-    </div>
-    <div>
-      <h3>Client</h3>
-      <p><?= $page->client() ?></p>
-    </div>
-    <div>
-      <h3>Role</h3>
-      <p><?= $page->role() ?></p>
-    </div>
-    <div>
-      <?php if($page->links()->toStructure()->count() > 1) : ?>
-        <h3>Links</h3>
-      <?php else: ?>
-        <h3>Link </h3>
-        <p>—</p>
-      <?php endif ?>
-      <?php $links = $page->links()->toStructure();
+    <hr>
+    <div class="grid-container">
+      <div>
+        <small>Date</small>
+        <p><?= $page->year()->toDate('m/Y') ?></p>
+      </div>
+      <div id="category">
+        <small>Category</small>
+        <?php foreach ($page->category()->split() as $category): ?>
+          <p><?= $category ?></p>
+        <?php endforeach ?>
+      </div>
+      <div id="client">
+        <small>Client</small>
+        <p><?= $page->client() ?></p>
+      </div>
+      <div>
+        <small>Role</small>
+        <p><?= $page->role() ?></p>
+      </div>
+      <div>
+        <small>Dimensions</small>
+        <p><?= $page->dimensions() ?></p>
+      </div>
+      <div>
+        <?php if($page->links()->toStructure()->count() > 1) : ?>
+          <small>Links</small>
+        <?php else: ?>
+          <small>Link </small>
+        <?php endif ?>
+        <?php if ($page->links()->isEmpty()): ?>
+          <p>—</p>
+        <?php else: ?>
+          <?php $links = $page->links()->toStructure();
           foreach ($links as $link) : ?>
             <div class="i i-5">
               <a href="<?= $link->target()->toLinkObject() ?>" target="_blank"><?= $link->title() ?></a>
             </div>
-    <?php endforeach ?>
+          <?php endforeach ?>
+      <?php endif ?>
+      </div>
     </div>
   </article>
-  
-  <article>
-    <figure>
-      <?= $page->image() ?>
-    </figure>
-  </article>
-  
-
-  <?= $page->blocks()->toBlocks() ?>
 
 </main>
 
