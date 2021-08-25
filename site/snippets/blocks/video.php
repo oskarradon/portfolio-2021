1f7efaec2ae210a9bee7b05bcd2074d7615ad58b
Oskar <oskarradonkimball@gmail.com>
2021-08-25 13:58:15 -0700

    Update video.php

--- site/snippets/blocks/video.php
+++ site/snippets/blocks/video.php
@@ -5,12 +5,17 @@ $caption = $block->caption();
 $src     = null;
 
 if ($block->location() == 'web') {
-    $src = $block->src();
+  if($block->youtube()->isNotEmpty()) {
+    $src = $block->youtube();
+  } else {
+    $src = $block->vimeo();
+  }
 } elseif ($video = $block->video()->toFile()) {
     $src = $video->url();
 }
 
 ?>
+
 <?php if ($src): ?>
   <article>
     <figure class="video-block">
@@ -21,7 +26,11 @@ if ($block->location() == 'web') {
       <?php else: ?>
         <div class="plyr__video-embed player">
           <iframe
-            src="<?= $src ?>?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
+          <?php if ($block->youtube()->isNotEmpty()): ?>
+            src="https://www.youtube.com/embed/<?= $src ?>?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1"
+          <?php else: ?>
+            src="https://player.vimeo.com/video/<?= $src ?>?loop=false&amp;byline=false&amp;portrait=false&amp;title=false&amp;speed=true&amp;transparent=0&amp;gesture=media"
+          <?php endif ?>
             allowfullscreen
             allowtransparency
             allow="autoplay"
@@ -31,5 +40,3 @@ if ($block->location() == 'web') {
     </figure>
   </article>
 <?php endif ?>
-
-
