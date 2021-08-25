93ddd8a620395dc4d35b6afa38b303e7995921dc
Oskar <oskarradonkimball@gmail.com>
2021-08-25 13:58:13 -0700

    Update footer.php

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,27 +1,24 @@
       <footer>
         <p>&copy; <span></span></p>
+        
         <nav>
           <a href="https://github.com/oskarradon/">Github</a>
-          <a href="https://www.are.na/oskar-radon">Are.na</a>
-          <a href="mailto:oskarradon@protonmail.com">Email</a>
-          <a href="https://special.fish/oskar">special.fish</a>
+          <a id="arena" href="https://www.are.na/oskar-radon">Are.na</a>
+          <a href="mailto:oskarradonkimball@gmail.com">Email</a>
+          <a id="fish" href="https://special.fish/oskar">special.fish</a>
           <a href="https://www.linkedin.com/in/oskarradon/">Linkedin</a>
         </nav>
-    </footer>
+
       </footer>
 
       <!-- Vendor JS -->
-      <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+      <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+      <script src="https://cdn.plyr.io/3.6.8/plyr.js"></script>
+      
       <!-- Author JS -->
-      <?= js('assets/js/vendor/basicLightbox.min.js') ?>
       <?= js('assets/js/site.js', ['type' => 'module']) ?>
-      <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-      <script>
-        window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-        ga('create', 'UA-XXXXX-Y', 'auto'); ga('set', 'anonymizeIp', true); ga('set', 'transport', 'beacon'); ga('send', 'pageview')
-      </script>
-      <script src="https://www.google-analytics.com/analytics.js" async></script>
+      
     </div>
   </body>
 </html>
